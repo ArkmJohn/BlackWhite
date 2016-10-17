c0a90b954f56175950b782397d0a6e5d0acc3da9
Sasan7 <s.faizollahzadeh@saedubai.com>
2016-10-17 12:59:00 +0400

    1

--- Assets/Scripts/CharacterClasses/PlayerClassSelectionScr.cs
+++ Assets/Scripts/CharacterClasses/PlayerClassSelectionScr.cs
@@ -4,12 +4,31 @@ using System.Collections;
 public class PlayerClassSelectionScr : MonoBehaviour {
 
     private string[] PlayerClass;
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private Player myPlayer;
+	private void Tank()
+    {
+       // myPlayer.statistics[0] 
+
+    }
+    private void DPSMele()
+    {
+
+
+    }
+    private void DPSRange()
+    {
+
+
+    }
+    private void BalancedRanged()
+    {
+
+
+    }
+    private void Balanced()
+    {
+
+
+    }
+
 }
