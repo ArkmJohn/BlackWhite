b85bdd6cdce8d8a25bf67884f2dc31766948912c
Sasan7 <s.faizollahzadeh@saedubai.com>
2016-10-25 14:29:54 +0400

    5

---
+++ Assets/Scripts/cameraScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraScript : MonoBehaviour {
+
+    public GameObject player;
+    
+    public float xDist = 10f;
+    public float yDist = 10f;
+    public float zDist = 10f;
+
+    private Vector3 camDistance;
+    //player will be attached to this
+    void Start () {
+        if(player == null)
+        {
+            //player = FindObjectOfType<Player>();
+            player = GameObject.FindGameObjectWithTag("Player");
+
+            camDistance = new Vector3(xDist, yDist, zDist);
+        }
+        
+	
+	}
+	
+	void Update () {
+        transform.position = player.transform.position + camDistance;
+        transform.LookAt(player.transform.forward);
+        //transform.rotation = player.transform.rotation;
+	}
+}
