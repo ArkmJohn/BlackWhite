e062180fb0a0637833ddcb727b5cd43ad42e23dc
rtanwar28 <r.tanwar@saedubai.com>
2016-11-14 13:06:58 +0400

    Avoid Wall Updates

--- Enemy.cs
+++ Enemy.cs
@@ -1,39 +1,117 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy : MonoBehaviour
+public class Enemy : Character
 {
-	
+    public Vector3 targetLocation;
+    public Tree myAITree;
+    public GameObject target;
+    public Vector3 velocity;
+    public Vector3 position;
+    public Node routine;
 
-	// Array to store the spawn point game objects
-	public GameObject[] spawnPoints = new GameObject[1];
+    public Vector3 linear;
+    public Vector3 rotation;
+	public Vector3 angular;
+    public float maxAccel;
+    public bool isDummy;
+	public bool isAvoidWall;
 
-	// Grunt enemy prefab
-	public GameObject gEnemyPrefab;
+	public Vector3 tempRotation;
 
-	// Commander enemy prefab
-	public GameObject cEnemyPrefab; 
+    public void Init()
+    {
+        routine.Start();
+    }
 
-	// To store the random number 
-	int pos;
+    // This is where the enemy should put all its ai
+    protected void Act()
+    {
+        //myAITree.AIAct();
+        // Do some other stuff
 
-	// Use this for initialization
-	void Start () 
+        if (Health > 0)
+        {
+            routine.act(this);
+        }
+    }
+
+    public void Tick()
+    {
+        Act();
+    }
+
+    void Update()
 	{
-		//AI_Wander wanderObj = GameObject.FindGameObjectWithTag ("GruntEnemy").GetComponent<AI_Wander> ();
-			
-		//wanderObj.hasSpawned = false;
-		// Generating a random number and storing the value in pos
-		pos = Random.Range (0, 4);
-		
-		// Instantiating the grunt enemy 
-		Instantiate(gEnemyPrefab, spawnPoints [pos].transform.position,Quaternion.identity);
-		//wanderObj.hasSpawned = true;
+
+        if (!isDummy)
+        {
+            Vector3 displacement = velocity * Time.deltaTime;
+
+            transform.Translate(displacement, Space.World);
+			float angle = Vector3.Angle (rotation, transform.position);
+			Debug.Log (angle);
+			transform.eulerAngles = new Vector3(0 , angle, 0);
+
+
+        }
+
+		if(isAvoidWall)
+		{
+			//linear = Vector3.zero;
+			//velocity = Vector3.zero;
+			//rotation = Vector3.zero;
+			//rotation = tempRotation;
+
+		}
+
+		if(!isAvoidWall)
+		{
+		//	linear = new Vector3(0,0,0);
+		//	velocity = new Vector3(1,0,1);
+
+			//rotation = tempRotation;
+			//transform.position += transform.forward * (2 * Time.deltaTime);
+		}
+
 	}
-	
-	// Update is called once per frame
-	void Update () 
+
+    public void LateUpdate()
+    {
+        if (!isDummy)
+        {
+			//linear += wallAvoidVIndent;
+            velocity += linear * Time.deltaTime;
+
+            // Calculates the speed if it is faster or slower than intended
+            if (velocity.magnitude > _speed)
+            {
+                velocity.Normalize();
+                velocity *= _speed;
+            }
+			//angular += wallAvoidIndent;
+			rotation = angular;
+
+            if (linear.sqrMagnitude == 0)
+                velocity = Vector3.zero;
+        }
+
+    }
+
+	void OnDrawGizmos()
 	{
-		
+		Gizmos.DrawSphere (velocity + transform.position, 1);
+		Gizmos.color = Color.red;
+		Gizmos.DrawSphere (rotation + transform.position, 1);
 	}
+
+    // TODO: Set animations and stuff
+    public void AttackAnim()
+    { }
+    public void WalkAnim()
+    { }
+    public void StandAnim()
+    { }
+    public void DieAnim()
+    { }
 }
---
+++ WallAvoid.cs
@@ -0,0 +1,152 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallAvoid : MonoBehaviour
+{
+
+	// Length of the ray
+	public int rayLength;
+
+	// Structure used to get information back from a raycast.
+	public RaycastHit hit;
+
+	//string layerName;
+	int layerMask;
+
+	public bool boolCheck;
+
+	Enemy me;
+
+	Vector3 direction;
+
+	public Transform originPoint;
+
+	public float rotateValue;
+	public Vector3 targetRotation;
+
+	public bool isRotating;
+	bool isUpdate;
+
+	float yVal;
+
+	// Use this for initialization
+	void Start () 
+	{
+		me = GetComponent<Enemy> ();
+		boolCheck = false;
+		isRotating = false;
+		isUpdate = true;
+		layerMask = LayerMask.NameToLayer ("AI");
+
+		yVal = 0f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// The direction for the ray
+	//	direction = originPoint.TransformDirection(Vector3.forward);
+
+
+
+		direction = me.velocity + me.transform.position + (transform.forward * 2);
+		
+		// Displaying the ray in the desired direction and position
+		Debug.DrawLine(me.transform.position, direction, Color.red);
+
+		if (isUpdate)
+		{
+			if (Physics.Raycast (transform.position, direction, out hit, rayLength)) 
+			{
+				if (hit.transform.gameObject.layer == layerMask) 
+				{
+					boolCheck = true;
+					me.isAvoidWall = true;
+					CheckLayer (boolCheck);
+				} 
+				else 
+				{
+					boolCheck = false;
+					CheckLayer (boolCheck);
+				}
+			}
+		}
+			
+		/*	if(isRotating)
+		{
+			
+		}*/
+	}
+
+	void CheckLayer(bool hasHit)
+	{		
+		if (hasHit) 
+		{
+			isRotating = false;
+			isUpdate = false;
+			SetRotation ();
+		}
+	}
+
+	void SetRotation()
+	{
+		CheckY ();
+
+		//me.angular += new Vector3(0,yVal,0);
+
+		//me.transform.eulerAngles = new Vector3 (0, yVal, 0);
+
+		Debug.Log ("me.transform.eulerAngles : " + me.transform.eulerAngles);
+
+		rotateValue = Random.Range (90f,180f);
+
+		Debug.Log ("Random value generated : " + rotateValue);
+
+		targetRotation = new Vector3 (0, rotateValue,0);
+
+		Debug.Log ("Vector3 RotateV : "+targetRotation);
+
+		Debug.Log ("me.transform.rotation.eulerAngles " + me.transform.rotation.eulerAngles + " - RotateV " + targetRotation);
+
+//		rotateV = me.transform.rotation.eulerAngles - rotateV;
+		targetRotation = targetRotation - me.transform.rotation.eulerAngles;
+
+		Debug.Log ("New Vector3 RotateV after subtraction: "+targetRotation);
+
+
+		//me.transform.eulerAngles = targetRotation;
+		me.angular += targetRotation;
+		me.tempRotation += targetRotation;
+
+
+		Debug.Log ("Temporary Rotation : " + me.tempRotation);
+
+		CheckY ();
+
+		Debug.Log ("Passing RotateV value to eulerAngles: " + me.transform.rotation.eulerAngles);
+
+		CheckY ();
+	
+		isRotating = true;
+
+		//Debug.Log ("transform.localeulerangles : " + me.transform.localEulerAngles); 
+	}
+
+	// To check and calculate the value of y-rotation if negative or positive.
+	void CheckY()
+	{
+
+		if (me.transform.rotation.eulerAngles.y > 180) {		
+			yVal = me.transform.rotation.eulerAngles.y - 360f;
+			Debug.Log ("Y (if negative): " + yVal);
+		}
+		else 
+		{
+			yVal = me.transform.rotation.eulerAngles.y;
+			Debug.Log ("Y (if positive): " + yVal);
+		}
+
+	}
+}
+
