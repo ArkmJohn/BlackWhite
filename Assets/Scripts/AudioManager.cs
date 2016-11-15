b74508fa4b92839045c0630379cb762a5890c783
Sasan7 <s.faizollahzadeh@saedubai.com>
2016-11-15 14:54:13 +0400

    commiting

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -3,28 +3,6 @@ using System.Collections;
 
 public class AudioManager : MonoBehaviour {
 
-    public AudioClip[] AudioFiles;
-    /*
-    0 = subtle punch
-    1 = fire searing
-    2 = Splash
-    3 = squish Sound
-    4 = Arrow Pull
-    5 = Breaking Vase
-    6 = electrocution
-    7 = cracking glass
-    8 = slice
-    9 = ArrowHitting
-    10 = Explosion
-    11 =  smash 
-    12 = sharp punch
-    13 = walking
-    14 = smash 2
-    15 = ping
-    16 = cartoonish punch
-
-    */
-
     public float volume;
     public void PlayEffect(AudioClip CurrAudio)
     {
